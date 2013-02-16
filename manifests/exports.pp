1ad9c690a3948191e59c10c4c8087dad2b102fa3
Lucas Bickel <hairmare@purplehaze.ch>
2013-02-16 14:57:51 +0100

    disable proxy-html mapping
    I'm doing this on the backend in mediawikis config since it work better
    that way.

--- manifests/exports.pp
+++ manifests/exports.pp
@@ -4,7 +4,6 @@ class mediawiki::exports {
   @@apache::vhost::include::proxy { "mediawiki_${fqdn}_default_ssl_reverseproxy":
     proxy_vhost => 'default_ssl_vhost',
     location    => '/mediawiki_test/',
-    url_map     => '/ /mediawiki_test/',
     dest        => "http://${fqdn}/",
     tag         => ['intranet', 'extranet']
   }
