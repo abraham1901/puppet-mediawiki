e5dc7cc64dbcdce9b23fa6a12dc13ad553f44403
Lucas Bickel <hairmare@purplehaze.ch>
2013-02-23 22:48:17 +0100

    change path on proxy

--- manifests/exports.pp
+++ manifests/exports.pp
@@ -3,7 +3,7 @@
 class mediawiki::exports {
   @@apache::vhost::include::proxy { "mediawiki_${fqdn}_default_ssl_reverseproxy":
     proxy_vhost => 'default_ssl_vhost',
-    location    => '/mediawiki_test/',
+    location    => '/mediawiki/',
     dest        => "http://${fqdn}/",
     tag         => ['intranet', 'extranet']
   }
