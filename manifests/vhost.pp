9ac9b3d2ab36df5ac9752b5a36ee7e256df3c49c
Lucas Bickel <hairmare@purplehaze.ch>
2013-04-28 21:06:53 +0200

    more workaournds for broken apache module
    somehow the apache module is broken for puppet-3. I seriously need to
    start planning how to get rid of puppetlabs-apache completely. I would
    like to replace it with a module that can be made to work with my
    CouchDB ENC in a proper way.

--- manifests/vhost.pp
+++ manifests/vhost.pp
@@ -1,22 +1,29 @@
 # == Class: mediawiki::vhost
 #
 # create vhost definition for mediawiki on $fqdn
-#
+#   
 class mediawiki::vhost {
   # @todo these file belong in apache::vhost somewhere
   file {
     "/var/www/${fqdn}":
       ensure => directory,
       mode   => '0755';
-
+  
     "/var/www/${fqdn}/htdocs":
       ensure => directory,
       mode   => '0755';
   } -> apache::vhost { 'mediawiki_vhost':
-    port       => 80,
-    docroot    => "/var/www/${fqdn}/htdocs",
-    template   => "apache/vhost-default.conf.erb",
-    servername => $fqdn;
+    port          => 80,
+    priority      => 25,
+    vhost_name    => '*',
+    serveraliases => [],
+    options       => 'Indexes FollowSymLinks MultiViews',
+    override      => ['None'],
+    apache_name   => 'apache',
+    docroot       => "/var/www/${fqdn}/htdocs",
+    template      => "apache/vhost-default.conf.erb",
+    logroot       => "/var/log/apache",
+    servername    => $fqdn;
   } -> Webapp_config['mediawiki']
 
-}
+} 
