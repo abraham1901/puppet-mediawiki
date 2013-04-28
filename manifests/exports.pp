f8299049407645c6eb0695b4ca515e9bdc109198
Lucas Bickel <hairmare@purplehaze.ch>
2013-04-29 01:09:44 +0200

    make proxy export path configurable

--- manifests/exports.pp
+++ manifests/exports.pp
@@ -3,7 +3,7 @@
 class mediawiki::exports {
   @@apache::vhost::include::proxy { "mediawiki_${fqdn}_default_ssl_reverseproxy":
     proxy_vhost => 'default_ssl_vhost',
-    location    => '/mediawiki/',
+    location    => $::mediawiki_export_vhost_location,
     dest        => "http://${fqdn}/",
     tag         => ['intranet', 'extranet']
   }
